2756431afc08de900f116b57d7ebd4c9950d0869
Igor Afonov <afonov@gmail.com>
2009-02-08 12:03:52 +0200

    Fixed block passing through new.

--- shitbrix.rb
+++ shitbrix.rb
@@ -34,20 +34,20 @@ class AbstractModule
 end
 
 class Class
-  def inject(variable)    
+  def inject(request)    
     if (@injection_requests == nil) then 
       @injection_requests = Array.new      
     end
   
-    @injection_requests << variable
+    @injection_requests << request
   end
 
   alias __new  new
    
-  def new(*args)     
-    obj = __new(*args)    
+  def new(*args, &block)
+    obj = __new(*args, &block)
 
-    if (@injection_requests != nil) then      
+    if (@injection_requests != nil) then
       @injection_requests.each do |request|         
         obj.instance_variable_set("@#{request}", ShitBrix.injector.get_instance(request))
       end      
